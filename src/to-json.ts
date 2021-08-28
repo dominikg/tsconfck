f64d4c7cdbcb619b46bc9a70bf8ab4f671959f76
dominikg <dominik.goepel@gmx.de>
2021-08-28 15:00:01 +0200

    fix: inline strip-bom and strip-json-comments to get cjs build working

--- src/to-json.ts
+++ src/to-json.ts
@@ -1,5 +1,18 @@
-import stripBom from 'strip-bom';
-import stripJsonComments from 'strip-json-comments';
+/*
+ this file contains code from strip-bom and strip-json-comments by Sindre Sorhus
+ https://github.com/sindresorhus/strip-json-comments/blob/v4.0.0/index.js
+ https://github.com/sindresorhus/strip-bom/blob/v5.0.0/index.js
+-- LICENSE --
+MIT License
+
+Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (https://sindresorhus.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 /**
  * convert content of tsconfig.json to regular json
@@ -10,7 +23,7 @@ import stripJsonComments from 'strip-json-comments';
 export function toJson(tsconfigJson: string): string {
 	const stripped = stripDanglingComma(stripJsonComments(stripBom(tsconfigJson)));
 	if (stripped.trim() === '') {
-		// only whitespace left after stripping, return empty object
+		// only whitespace left after stripping, return empty object so that JSON.parse still works
 		return '{}\n';
 	} else {
 		return stripped;
@@ -22,16 +35,6 @@ export function toJson(tsconfigJson: string): string {
  * implementation heavily inspired by strip-json-comments
  */
 function stripDanglingComma(pseudoJson: string) {
-	function isEscaped(jsonString: string, quotePosition: number) {
-		let index = quotePosition - 1;
-		let backslashCount = 0;
-		while (jsonString[index] === '\\') {
-			index -= 1;
-			backslashCount += 1;
-		}
-		return backslashCount % 2 === 1;
-	}
-
 	let insideString = false;
 	let offset = 0;
 	let result = '';
@@ -64,3 +67,86 @@ function stripDanglingComma(pseudoJson: string) {
 	}
 	return result + pseudoJson.substring(offset);
 }
+
+// start strip-json-comments
+function isEscaped(jsonString: string, quotePosition: number) {
+	let index = quotePosition - 1;
+	let backslashCount = 0;
+
+	while (jsonString[index] === '\\') {
+		index -= 1;
+		backslashCount += 1;
+	}
+
+	return Boolean(backslashCount % 2);
+}
+
+function strip(string: string, start?: number, end?: number) {
+	return string.slice(start, end).replace(/\S/g, ' ');
+}
+
+const singleComment = Symbol('singleComment');
+const multiComment = Symbol('multiComment');
+
+function stripJsonComments(jsonString: string) {
+	let isInsideString = false;
+	let isInsideComment: false | symbol = false;
+	let offset = 0;
+	let result = '';
+
+	for (let index = 0; index < jsonString.length; index++) {
+		const currentCharacter = jsonString[index];
+		const nextCharacter = jsonString[index + 1];
+
+		if (!isInsideComment && currentCharacter === '"') {
+			const escaped = isEscaped(jsonString, index);
+			if (!escaped) {
+				isInsideString = !isInsideString;
+			}
+		}
+
+		if (isInsideString) {
+			continue;
+		}
+
+		if (!isInsideComment && currentCharacter + nextCharacter === '//') {
+			result += jsonString.slice(offset, index);
+			offset = index;
+			isInsideComment = singleComment;
+			index++;
+		} else if (isInsideComment === singleComment && currentCharacter + nextCharacter === '\r\n') {
+			index++;
+			isInsideComment = false;
+			result += strip(jsonString, offset, index);
+			offset = index;
+		} else if (isInsideComment === singleComment && currentCharacter === '\n') {
+			isInsideComment = false;
+			result += strip(jsonString, offset, index);
+			offset = index;
+		} else if (!isInsideComment && currentCharacter + nextCharacter === '/*') {
+			result += jsonString.slice(offset, index);
+			offset = index;
+			isInsideComment = multiComment;
+			index++;
+		} else if (isInsideComment === multiComment && currentCharacter + nextCharacter === '*/') {
+			index++;
+			isInsideComment = false;
+			result += strip(jsonString, offset, index + 1);
+			offset = index + 1;
+		}
+	}
+
+	return result + (isInsideComment ? strip(jsonString.slice(offset)) : jsonString.slice(offset));
+}
+// end strip-json-comments
+
+// start strip-bom
+function stripBom(string: string) {
+	// Catches EFBBBF (UTF-8 BOM) because the buffer-to-string
+	// conversion translates it to FEFF (UTF-16 BOM).
+	if (string.charCodeAt(0) === 0xfeff) {
+		return string.slice(1);
+	}
+	return string;
+}
+// end strip-bom
