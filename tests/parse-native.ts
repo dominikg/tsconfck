77ec1deb6a1c1f4c44a939c9c259fd96c93d6ba2
dominikg <dominik.goepel@gmx.de>
2022-03-23 21:36:01 +0100

    fix: fix tests by ignoring dir/tsconfig.json in invalid fixtures and update expected result

--- tests/parse-native.ts
+++ tests/parse-native.ts
@@ -237,7 +237,10 @@ test('should resolve with tsconfig that works when transpiling', async () => {
 });
 
 test('should reject with correct error position for invalid tsconfig.json', async () => {
-	const samples = await glob('tests/fixtures/parse/invalid/**/tsconfig.json');
+	let samples = await glob('tests/fixtures/parse/invalid/**/tsconfig.json');
+	samples = samples.filter(
+		(sample) => !sample.includes(path.join('extends-fallback-not-found', 'dir'))
+	);
 	for (const filename of samples) {
 		const expected = await loadExpectedJSON(filename, 'expected.native.json');
 		try {
